daec428704f32962c0c362fb7821e3805ad69c74
Niorth <hugo.niort9@gmail.com>
2020-01-04 23:53:17 +0100

    glider creation error

--- src/View/Controller/GliderCreationController.java
+++ src/View/Controller/GliderCreationController.java
@@ -1,11 +1,16 @@
 package View.Controller;
 
 import Facade.GliderFacade;
+import View.Main.Main;
 import javafx.event.ActionEvent;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.io.IOException;
+
 public class GliderCreationController {
+    public Label errorLabel;
     private GliderFacade gliderFacade;
     public DatePicker reviewDateInput;
     public TextField registrationIDInput;
@@ -15,14 +20,25 @@ public class GliderCreationController {
     public GliderCreationController() { this.gliderFacade = new GliderFacade();}
 
     public void createGlider(ActionEvent actionEvent) {
-        boolean done = gliderFacade.createGlider(
-                registrationIDInput.getText(),
-                Float.parseFloat(spanInput.getText()),
-                Float.parseFloat(maxWidthInput.getText()),
-                java.sql.Date.valueOf(reviewDateInput.getValue()),
-                1
-        );
-
-        System.out.println(done);
+        if(registrationIDInput.getText().equals("") || spanInput.getText().equals("") || maxWidthInput.getText().equals("") || reviewDateInput.getValue() == null) {
+            errorLabel.setText("Input Error");
+        } else {
+            boolean done = gliderFacade.createGlider(
+                    registrationIDInput.getText(),
+                    Float.parseFloat(spanInput.getText()),
+                    Float.parseFloat(maxWidthInput.getText()),
+                    java.sql.Date.valueOf(reviewDateInput.getValue()),
+                    1
+            );
+            if(!done) {
+                errorLabel.setText("Creation Error");
+            } else {
+                try {
+                    Main.glidersView(Main.getPrimaryStage());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
