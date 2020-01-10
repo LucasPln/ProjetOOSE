35b96d497ab1da4bd53767b087d0af3a57427c67
LucasPln <lucas.paulin@hotmail.fr>
2020-01-10 18:56:59 +0100

    Ajout des élement dans la page addBooking

--- src/View/Controller/AddBookingUIController.java
+++ src/View/Controller/AddBookingUIController.java
@@ -1,12 +1,48 @@
 package View.Controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
 
 public class AddBookingUIController {
 
     @FXML
+    private DatePicker startDateField;
+
+    @FXML
+    private DatePicker endDateField;
+
+    @FXML
+    private ChoiceBox batteryField;
+
+    @FXML
+    private ChoiceBox GPSField;
+
+    @FXML
+    private ChoiceBox launcherPlaneField;
+
+    @FXML
+    private ChoiceBox launcherWinchField;
+
+    @FXML
+    private ChoiceBox launchmanField;
+
+    @FXML
+    private ChoiceBox flightManagerField;
+
+    @FXML
+    private ChoiceBox diplomaField;
+
+    @FXML
+    private ChoiceBox gliderField;
+
+    @FXML
+    private Button addButton;
+
+    @FXML
     public void initialize() {
-        
+
     }
 
 }
