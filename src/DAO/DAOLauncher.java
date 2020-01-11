c167f0789ffc86d06b074f5af815cc6a95011137
LucasPln <lucas.paulin@hotmail.fr>
2020-01-11 16:14:28 +0100

    ajout de la méthode getAllPlane

--- src/DAO/DAOLauncher.java
+++ src/DAO/DAOLauncher.java
@@ -1,5 +1,11 @@
 package DAO;
 
+import Model.Plane;
+
+import java.util.ArrayList;
+
 public interface DAOLauncher {
+
+    ArrayList<Plane> getAllPlane();
 
 }
--- src/DAO/DAOSqlLauncher.java
+++ src/DAO/DAOSqlLauncher.java
@@ -1,4 +1,12 @@
 package DAO;
 
+import Model.Plane;
+
+import java.util.ArrayList;
+
 public class DAOSqlLauncher implements DAOLauncher {
+    @Override
+    public ArrayList<Plane> getAllPlane() {
+        return null;
+    }
 }
