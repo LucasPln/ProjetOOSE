ea3a6c0f8dd4f1d9a2ef00c9785c5d273dee753c
arnaudmaesboyer <43748615+arnaudmaesboyer@users.noreply.github.com>
2020-01-12 15:06:13 +0100

    recuperation gliders dasn company

--- src/Facade/CompanyFacade.java
+++ src/Facade/CompanyFacade.java
@@ -2,7 +2,10 @@ package Facade;
 
 import DAO.AbstractFactory;
 import Model.Company;
+import Model.Glider;
 import Model.User;
+
+import java.util.ArrayList;
 
 public class CompanyFacade {
 
@@ -13,12 +16,15 @@ public class CompanyFacade {
         public CompanyFacade() {
             this.abstractFactory = AbstractFactory.getInstance();
             this.abstractFactory.createCompanyDAO();
+            this.abstractFactory.createGliderDAO();
             System.out.println("Company créé");
         }
 
         public Company getInfos(int idC){
             return abstractFactory.getCompanyDAO().infos(idC);
         }
+
+        public ArrayList<Glider> getAllGlidersCompany(int idC) {return abstractFactory.getDaoGlider().getGlidersFromCompany(idC);}
 
     }
 
--- src/View/Controller/CompanyUIController.java
+++ src/View/Controller/CompanyUIController.java
@@ -3,13 +3,22 @@ package View.Controller;
 import Facade.CompanyFacade;
 import Facade.LoginFacade;
 import Model.Company;
+import Model.Glider;
+import View.Main.Main;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class CompanyUIController {
 
         @FXML
         private Label nomCompany;
+        @FXML
+        private Button returnBtn;
 
         public CompanyUIController(){
                 companyFacade = new CompanyFacade();}
@@ -18,10 +27,19 @@ public class CompanyUIController {
         @FXML
         public void initialize() {
                 int id = LoginFacade.getInstance().getConnectedUser().getCompanyId();
-                System.out.println(id);
                 Company c =companyFacade.getInfos(id);
-                System.out.println(c);
+                System.out.println(id);
+                ArrayList<Glider> GliderList = companyFacade.getAllGlidersCompany(id);
+                System.out.println(GliderList);
+
                 nomCompany.setText(c.getName());
+        }
+        public void ReturnView(ActionEvent actionEvent) {
+                try {
+                        Main.homeView(Main.getPrimaryStage());
+                } catch (IOException e) {
+                        e.printStackTrace();
+                }
         }
     }
 
