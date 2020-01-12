8c6dd4b08ff7ab46603f9138ce0cafe049c68ddd
arnaudmaesboyer <43748615+arnaudmaesboyer@users.noreply.github.com>
2020-01-12 16:03:06 +0100

    Affichage glider

--- src/View/Controller/CompanyUIController.java
+++ src/View/Controller/CompanyUIController.java
@@ -5,10 +5,13 @@ import Facade.LoginFacade;
 import Model.Company;
 import Model.Glider;
 import View.Main.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -19,6 +22,8 @@ public class CompanyUIController {
         private Label nomCompany;
         @FXML
         private Button returnBtn;
+        @FXML
+        private ListView listGlider;
 
         public CompanyUIController(){
                 companyFacade = new CompanyFacade();}
@@ -28,11 +33,16 @@ public class CompanyUIController {
         public void initialize() {
                 int id = LoginFacade.getInstance().getConnectedUser().getCompanyId();
                 Company c =companyFacade.getInfos(id);
-                System.out.println(id);
-                ArrayList<Glider> GliderList = companyFacade.getAllGlidersCompany(id);
-                System.out.println(GliderList);
-
                 nomCompany.setText(c.getName());
+
+                ArrayList<Glider> GliderList = companyFacade.getAllGlidersCompany(id);
+                ObservableList<String> gliders =FXCollections.observableArrayList();
+                for(int i = 0 ; i < GliderList.size(); i++) {
+                        String planeur = GliderList.get(i).getRegistrationGlider()+"  "+ GliderList.get(i).getSpan()+"  "+ GliderList.get(i).getMaxWeight()+"  "+GliderList.get(i).getAcquisitionDate()+"  "+GliderList.get(i).getReviewDate();
+                        gliders.add(planeur);
+                }
+                listGlider.setItems(gliders);
+
         }
         public void ReturnView(ActionEvent actionEvent) {
                 try {
