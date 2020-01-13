f791a8e18d638f4571d9abaecb7d86ffb85dda12
paolaANDREU <paola.andreu@etu.umontpellier.fr>
2020-01-13 16:06:33 +0100

    view users fix bugs

--- src/View/Controller/ReadUserUIController.java
+++ src/View/Controller/ReadUserUIController.java
@@ -2,14 +2,14 @@ package View.Controller;
 
 import Facade.ReadUserFacade;
 import Model.User;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
+
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
 import java.util.ArrayList;
+
 
 public class ReadUserUIController {
     @FXML
@@ -19,13 +19,13 @@ public class ReadUserUIController {
     private ChoiceBox choiceBox;
     private ReadUserFacade readUserFacade;
 
-
     public ReadUserUIController(){
         this.readUserFacade = new ReadUserFacade();
     }
 
     @FXML
     public void initialize() {
+
         ArrayList<User> users = this.readUserFacade.getUsersByRole("Monitor");
         for(int i = 0; i < users.size(); i++) {
 
@@ -62,7 +62,8 @@ public class ReadUserUIController {
 
     @FXML
     public void getUsers() {
-        newGridPane();
+        gridUsers.getChildren().clear();
+        initLabels();
         String role = choiceBox.getValue().toString();
         ArrayList<User> users = this.readUserFacade.getUsersByRole(role);
         for(int i = 0; i < users.size(); i++) {
@@ -96,14 +97,38 @@ public class ReadUserUIController {
             GridPane.setHalignment(l7, javafx.geometry.HPos.CENTER);
 
         }
+        gridUsers.setStyle("-fx-background-fill: black, white; -fx-background-insets: 0,1;");
     }
-    public void newGridPane(){
-        ObservableList<Node> fils = this.gridUsers.getChildren();
-        for (int i=8;i< fils.size();i++){
-            System.out.println(fils.get(i).toString());
-            this.gridUsers.getChildren().remove(fils.get(i));
 
-        }
+    public void initLabels(){
+        Label l1= new Label("Firstname");
+        gridUsers.add(l1,0, 0);
+        GridPane.setHalignment(l1, javafx.geometry.HPos.CENTER);
+
+        Label l2 = new Label("Lastname");
+        gridUsers.add(l2,1, 0);
+        GridPane.setHalignment(l2, javafx.geometry.HPos.CENTER);
+
+        Label l3 = new Label("Birth Date");
+        gridUsers.add(l3,2, 0);
+        GridPane.setHalignment(l3, javafx.geometry.HPos.CENTER);
+
+        Label l4 = new Label("Adress");
+        gridUsers.add(l4,3, 0);
+        GridPane.setHalignment(l4, javafx.geometry.HPos.CENTER);
+
+        Label l5 = new Label("Postal Code");
+        gridUsers.add(l5,4, 0);
+        GridPane.setHalignment(l5, javafx.geometry.HPos.CENTER);
+
+        Label l6 = new Label("Mail");
+        gridUsers.add(l6,5, 0);
+        GridPane.setHalignment(l6, javafx.geometry.HPos.CENTER);
+
+        Label l7 = new Label("Tel");
+        gridUsers.add(l7,6, 0);
+        GridPane.setHalignment(l7, javafx.geometry.HPos.CENTER);
+
     }
 
 }
